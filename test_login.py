220d621c639912063551d46514d97357dde509f5
Hanna Burianova <hburianova@lvl1807001.lohika.com>
2018-12-14 17:28:18 +0200

    Invalid tests

--- test_login.py
+++ test_login.py
@@ -17,3 +17,16 @@ def test_login_invalid_password():
     res = requests.post("https://python-qa-training.herokuapp.com/api/auth/login",
                  json={"email": "hburianova@lohika.com", "password": "678"})
     assert res.status_code == 401
+
+
+def test_login_empty_credentials():
+    res = requests.post("https://python-qa-training.herokuapp.com/api/auth/login",
+                 json={"email": "", "password": ""})
+    assert res.status_code == 401
+
+
+def test_login_invalid_credentials():
+    res = requests.post("https://python-qa-training.herokuapp.com/api/auth/login",
+                 json={"email": "anna@anna", "password": "809"})
+    assert res.status_code == 401
+
